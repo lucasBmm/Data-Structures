ee546f71e20964e25b07ce54164bbf12bd20e26b
lucasBmm <lucassbomfimm@gmail.com>
2022-10-26 20:47:10 -0300

    added find and insertAt methods to linked list

--- Linked Lists/LinkedList.ts
+++ Linked Lists/LinkedList.ts
@@ -17,6 +17,11 @@ class LinkedList<T> {
         this.size = 0;
     }
 
+    /**
+     * Add an element at the beggining of the Linked List.
+     *
+     * @param data      Value to be inserted.
+     */
     add(data: T): void {
         let newNode = new LinkedListNode<T>(data);
 
@@ -28,6 +33,85 @@ class LinkedList<T> {
 
         [this.head, newNode.next] = [newNode, this.head];
         this.size++;
+    }
+
+    /**
+     * Add an element at the end of the Linked List.
+     *
+     * @param data      Value to be inserted.
+     */
+    append(data: T): void {
+        let newNode = new LinkedListNode<T>(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.size++;
+            return;
+        }
+
+        let n = this.head;
+
+        while(n.next) {
+            n = n.next;
+        }
+
+        n.next = newNode;
+        this.size++;
+    }
+
+    /**
+     * Add an element at determined index
+     *
+     * @param location  The index you want to insert.
+     * @param data      Value to be inserted.
+     */
+
+    insertAt(location: number, data: T): void {
+        let newNode = new LinkedListNode<T>(data);
+
+        if (location > this.size) {
+            throw new Error("Insert local must not be greater then the Linked List");
+        }
+
+        if (location === 0) {
+            this.add(data);
+            return;
+        }
+
+        if (location === this.size) {
+            this.append(data);
+            return;
+        }
+
+        let n = this.head;
+
+        for (let i = 1; i < location; i++) {
+            n = n?.next!;
+        }
+
+        if (n?.next) [n.next, newNode.next] = [newNode, n.next];
+        this.size++;
+    }
+    
+    /**
+     * Find a value of a node at determined index.
+     *
+     * @param index  The index of the node you want to find.
+     */
+    find(index: number): T {
+        let n = this.head;
+
+        if (index >= this.size) {
+            throw new Error("Index must be less then the size of the linked list")
+        }
+
+        for (let i = 0; i < index; i++) {
+            n = n?.next!;
+        }
+
+        let value = n?.value!;
+
+        return value;
     }
 
     toString(): String {
--- Linked Lists/main.test.ts
+++ Linked Lists/main.test.ts
@@ -1,18 +1,72 @@
 import LinkedList  from './LinkedList';
 
-test("Should create a linkedList", () => {
-    const list = new LinkedList<Number>;
+describe("Linked List tests", () => {
+    it("Should create a linkedList", () => {
+        const list = new LinkedList<Number>;
+    
+        expect(list.head).toBeNull();
+        expect(list.size).toBe(0);
+    });
+    
+    it("Should add new nodes to the beginning of the Linked List", () => {
+        const list = new LinkedList<Number>;
+    
+        list.add(1);
+        list.add(2);
+    
+        expect(list.size).toBe(2);
+        expect(list.toString()).toBe("2,1");
+    });
+    
+    it("Should add new nodes at the end of the Linked List", () => {
+        const list = new LinkedList<Number>;
+    
+        list.append(1);
+        list.append(2);
+    
+        expect(list.size).toBe(2);
+        expect(list.toString()).toBe("1,2");
+    });
+    
+    it("Should add at determined place", () => {
+        const list = new LinkedList<Number>;
+    
+        list.append(1);
+        list.append(2);
+        list.append(4);
+    
+        list.insertAt(2, 3);
+    
+        expect(() => list.insertAt(10, 1)).toThrowError();
+        expect(list.size).toBe(4);
+        expect(list.toString()).toBe("1,2,3,4");
+    });
 
-    expect(list.head).toBeNull();
-    expect(list.size).toBe(0);
-})
+    it("Should correct add to beggining or end of the linked list", () => {
+        const list = new LinkedList<Number>;
 
-test("Should add new nodes to the beginning of the Linked List", () => {
-    const list = new LinkedList<Number>;
+        for (let i = 0; i < 8; i++) {
+            list.append(i+1);
+        }
 
-    list.add(1);
-    list.add(2);
+        list.insertAt(0, 0);  
+        list.insertAt(9, 9); 
+        list.insertAt(4, 11);
 
-    expect(list.size).toBe(2);
-    expect(list.toString()).toBe("2,1");
+        expect(list.size).toBe(11);
+        expect(list.toString()).toBe("0,1,2,3,11,4,5,6,7,8,9");
+    });
+
+    it("Should get an element of the linked list at determined index", () => {
+        const list = new LinkedList<Number>;
+
+        for (let i = 10; i > 0; i--) {
+            list.append(i);
+        }
+
+        expect(list.find(0)).toBe(10);
+        expect(list.find(5)).toBe(5);
+        expect(list.find(9)).toBe(1);
+        expect(() => list.find(100)).toThrowError();
+    })
 })
